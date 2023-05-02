64f82d2e838cf1aab7d584ebe7ea22efeb061226
Ofek Kasif <ofekkasif@nova.cs.tau.ac.il>
2023-05-02 12:02:03 +0300

    m

--- myshell.c
+++ myshell.c
@@ -7,6 +7,20 @@
 #include <signal.h>
 #include <unistd.h>
 
+
+void print_error_and_exit(){
+    fprintf(stderr,"%s\n", stderror(errno));
+    exit(1);
+}
+
+pid_t fork_and_check_for_error(){
+    int ppid = fork();
+    if (ppid < 0){
+        print_error_and_exit();
+    } 
+    return ppid;
+}
+
 int* find_special_symbol(int count, char** arglist)
 /*scans the argument line for one of the following symbols: "%","|", ">". 
 will return a 2 elements array, first element will indicate the symbol and second eill indicate it's location at the command.
@@ -39,23 +53,60 @@ first element value: 0 - no special discription, 1 - "&", 2 - "|", 3 - "<"
 int prepare(void){
     return 0;
 }
+
 int process_arglist(int count, char** arglist){
-    pid_t ppid;
+    pid_t ppid_1, ppid_2;
+    int pipefd[2];
     int* symbol = find_special_symbol(count, arglist);
+
     if (symbol[0] == 0){
-        ppid = fork()
-        if (ppid < 0){
-            perror("An Error Has Occurred");
-            exit(1);
-        }
-        if (fork == 0){
+        ppid_1 = fork_and_check_for_error();  
+        if (ppid_1 == 0){
             if (execvp(arglist[0],arglist) == -1){
-                perror("An Error Has Occurred");
-                exit(1);
+                print_error_and_exit();
             }
         }
-        wait(NULL);
-        return 0;
+        else{
+            if(wait(NULL) < 0){
+                print_error_and_exit();
+            }
+            return 0;
+        }
+    }
+
+    else if (symbol[0] == 1){
+        ppid_1 = fork_and_check_for_error();
+        if (ppid_1 == 0){
+            arglist[count] = NULL;
+            if (execvp(arglist[0],arglist) == -1){
+                print_error_and_exit();
+            }
+        }
+    }
+
+    else{
+        arglist[symbol[1]] == NULL;
+
+        if (symbol[0] == 2){
+            if (pipe(pipefd) < 0){
+                print_error_and_exit();
+            }
+
+            ppid_1 = fork_and_check_for_error(); 
+
+            if (ppid_1 == 0){
+                dup2(pipefd[1], STDOUT_FILENO);
+                if (execvp(arglist[0],arglist) == -1){
+                    print_error_and_exit();
+                }
+            }
+
+            else {
+                ppid_2 = fork_and_check_for_error(); 
+                
+            }
+
+        }
     }
 
 }
